3b0476c154f4ea7c02dcb2a1d44aae4a3433d234
Eliav Menachi <eliav.menachi@gmail.com>
2024-12-30 16:21:00 +0200

    layout

--- src/PostsList.tsx
+++ src/PostsList.tsx
@@ -1,18 +1,53 @@
-function PostsList() {
-    const items = [
-        "An item",
-        "A second item",
-        "A third item",
-        "A fourth item",
-        "And a fifth one",
-    ]
+import { useState, FC } from "react";
+import PostListStyle from "./PostList.module.css"
+
+interface PostsListProps {
+    title: string,
+    items: string[],
+    onItemSelected: (index: number) => void
+}
+
+
+const PostsList: FC<PostsListProps> = ({ title, items, onItemSelected }) => {
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [render, setRender] = useState(0)
+
+    console.log("PostsList render")
+
+    const onClick = (index: number) => {
+        console.log("click " + index)
+        setSelectedIndex(index)
+    }
+
+    const addItem = () => {
+        console.log("add item")
+        items.push("New item")
+        setRender(render + 1)
+    }
+
+    const onSelect = () => {
+        onItemSelected(selectedIndex)
+    }
     return (
-        <div>
-            <ul className="list-group">
-                {items.map((item, index) => { return <li className="list-group-item">{index} {item}</li> })}
+        <div className={PostListStyle.container}>
+            <h1>{title}</h1>
+            {items.length == 0 && <p>No items</p>}
+            {items.length != 0 && <ul className="list-group">
+                {items.map((item, index) => {
+                    return <li
+                        className={selectedIndex == index ? 'list-group-item active' : 'list-group-item'}
+                        key={index}
+                        onClick={() => { onClick(index) }}>
+                        {index} {item}
+                    </li>
+                })}
             </ul>
+            }
+            <button className={'btn btn-primary m-3'} onClick={() => { addItem() }}>Add</button>
+            <button className={'btn btn-primary'} onClick={() => { onSelect() }}>Select</button>
         </div>
     )
+
 }
 
 export default PostsList
